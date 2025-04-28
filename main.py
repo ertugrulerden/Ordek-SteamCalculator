48010a7d9b92024d36cc3b87d743e1292732f8a5
Mac <mac@Mac-MacBook-Pro.local>
2025-04-28 22:07:20 +0300

    fixed images

--- main.py
+++ main.py
@@ -4,47 +4,26 @@ from tkinter import *
 
 from PIL import ImageTk, Image
 import os
-import requests
-from io import BytesIO
-
-
-img_url1 = "https://media.discordapp.net/attachments/644219379827081228/1057038430783143936/ordek.png?width=72&height=72"
-response1 = requests.get(img_url1)
-img_data1 = response1.content
-
-img_url2 = "https://cdn.discordapp.com/attachments/644219379827081228/1058103218233692181/madduck-removebg_4.png"
-response2 = requests.get(img_url2)
-img_data2 = response2.content
-
-def getInputBoxValue():
-	userInput = tInputBir.get()
-	return userInput
-
-def getInputBoxValue():
-	userInput = tInputIki.get()
-	return userInput
-
-
 
 root = Tk()
 root.eval('tk::PlaceWindow . center')
 root.resizable(False, False)
 
+img1 = Image.open("duck.png").resize((72, 72), Image.LANCZOS)
+img1 = ImageTk.PhotoImage(img1)
+ordek = tk.Label(root, image=img1, bg='#2F4F4F')
+ordek.place(x=182,y=75)
+
+madduckIMG = Image.open("gunDuck.png").resize((72, 72), Image.LANCZOS)
+madduckIMG = ImageTk.PhotoImage(madduckIMG)
+madduck = tk.Label(root, image=madduckIMG, bg='#2F4F4F')
+
 root.geometry('500x150')
 root.configure(background='#2F4F4F')
 root.title('ORDEK Hesaplayici')
 
-img1 = ImageTk.PhotoImage(Image.open(BytesIO(img_data1)))
-ordek = tk.Label(root, image=img1, bg='#2F4F4F')
-ordek.place(x=182,y=75)
-
-madduckIMG = ImageTk.PhotoImage(Image.open(BytesIO(img_data2)))
-madduck = tk.Label(root, image=madduckIMG, bg='#2F4F4F')
-
-
 etobuto = tk.Label(root, text='EtoButo#0001', bg='#2F4F4F', font=('helvetica', 9, 'normal'))
 etobuto.place(x=18,y=18)
-
 
 yazi1 = tk.Label(root, text='Steam Fiyati', bg='#2F4F4F', font=('helvetica', 13, 'normal', 'underline'))
 yazi2 = tk.Label(root, text='% Yuzdesi', bg='#2F4F4F', font=('helvetica', 13, 'normal', 'underline'))
@@ -54,14 +33,11 @@ yazi1.place(x=110, y=17)
 yazi2.place(x=230, y=17)
 yazi3.place(x=350,y=17)
 
-
 tInputBir=Entry(root,width=7, font=('helvetica', 12, 'bold'))
 tInputBir.place(x=125, y=60)
 
-
 tInputIki=Entry(root,width=7, font=('helvetica', 12, 'bold'))
 tInputIki.place(x=240, y=61)
-
 
 TL_Yazisi = tk.Label(root, text='', bg='#2F4F4F', font=('helvetica', 12, 'bold'))
 TL_Yazisi.place(x=400, y=105)
@@ -104,18 +80,15 @@ def change_text():
         madduck.place(x=182,y=75)
         
 
-
 always_on_top = tk.BooleanVar()
 
 def update_always_on_top():
     root.attributes("-topmost", always_on_top.get())
 
-
 Button(root, text='Hesapla', bg='#696969', font=('arial', 12, 'normal'), command=change_text).place(x=26, y=55)
 
 always_on_top_checkbox = tk.Checkbutton(root, text="Ustte Kal", font=('arial', 9, 'normal'), bg='#696969', variable=always_on_top, command=update_always_on_top)
 always_on_top_checkbox.place(x=26,y=100)
-
 
 temizle = Button(root, text='sil', bg='#696969', font=('arial', 9, 'normal'), command=clrText)
 temizle.place(x=120,y=100)
